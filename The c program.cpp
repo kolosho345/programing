1c0180f99e28d3eac8397ca4c78f463492d81412
kolosho345 <kolosho.28841@students.kyu.ac.ke>
2025-09-24 17:24:30 +0300

    Add files via upload

---
+++ The c program.cpp
@@ -0,0 +1,27 @@
+/* 
+name:ibrahim osman
+Reg No:PA106/G/28841/25
+Description:program for calculation of volume and surface area of cylinder  
+*/      
+#include<stdio.h>
+
+int main(){
+const double PI=3.14159;
+double radius;
+double height;
+double volume;
+double surfacearea;
+
+printf("enter radius \n");
+scanf("%lf", &radius);
+printf("enter height\n");
+scanf("%lf",&height);
+
+volume=PI*radius*radius*height;
+surfacearea=2*PI*radius*radius + 2*PI*radius*height;
+
+printf("volume:%lf\n",volume);
+printf("surfacearea:%lf\n",surfacearea);
+
+return 0;
+}
